893c75b98192c37449e4b921092ea8fa8925580c
Ankita Bansal <ankitabansal924@gmail.com>
2025-09-09 10:17:37 +0530

    Lower and upper bound using binary search

---
+++ src/BinarySearch/UpperBound.java
@@ -0,0 +1,24 @@
+package BinarySearch;
+
+public class UpperBound {
+    public static int upperBound(int[] arr, int target){
+        int n = arr.length;
+        int ans = n;
+        int low = 0, high = n - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(arr[mid] > target){
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 6, 8, 9};
+        System.out.println(upperBound(arr, 2));
+    }
+}
